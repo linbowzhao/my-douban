d4b41524992468f7b4c9ec52ac20dfef607c91b2
bobo <linbow@foxmail.com>
2017-09-07 10:57:40 +0800

    完成更多电影滑动申请数据

--- src/components/moreMovie.vue
+++ src/components/moreMovie.vue
@@ -16,6 +16,12 @@
   export default {
     name: 'moreMovie',
 
+    data: function () {
+      return {
+        scrollFlag: false
+      }
+    },
+
     components: {
       TopHeader,
       MovieItem
@@ -24,9 +30,42 @@
     computed: mapState({
       moreMovie (state) {
         var title = this.$route.params.title
+        console.log(state.moreMovie.moreCurrentMovie[title])
         return state.moreMovie.moreCurrentMovie[title]
       }
-    })
+    }),
+    created () {
+      window.addEventListener('scroll', this.bottomScrol)
+    },
+
+    destroyed () {
+      window.removeEventListener('scroll', this.bottomScrol)
+    },
+    methods: {
+      scrolling () {
+        var title = this.moreMovie.title
+        var count = this.moreMovie.count || this.moreMovie.subjects.length
+        count = count + 9
+        if (this.moreMovie.total && count < this.moreMovie.total) {
+          if (!this.scrollFlag) {
+            this.scrollFlag = true
+            this.$store.dispatch('getMoreData', {title: title, count: count, start: 0})
+            setTimeout((that) => { that.scrollFlag = false }, 500, this)
+          }
+        }
+      },
+      bottomScrol () {
+        var top = document.documentElement.scrollTop
+        if (top === 0) {
+          top = document.body.scrollTop
+        }
+        var height = document.getElementById('app').clientHeight
+        var innerHeight = window.innerHeight
+        if ((top + innerHeight) / height > 0.6) {
+          this.scrolling()
+        }
+      }
+    }
 
   }
 </script>
--- src/store/modules/moreMovie.js
+++ src/store/modules/moreMovie.js
@@ -1,12 +1,72 @@
 import * as types from '../mutation-types'
-
+import {
+  hotMovie,
+  commingSoon,
+  top250,
+  usBox,
+  weekly,
+  newMovies
+} from '../../router/server'
 const state = {
   moreCurrentMovie: {}
 }
 
+var moreAjax = (title, count, start) => {
+  switch (title) {
+    case '正在上映的电影-北京':
+      return hotMovie(count, start)
+    case '即将上映的电影':
+      return commingSoon(count, start)
+    case '豆瓣电影Top250':
+      return top250(count, start)
+    case '豆瓣电影北美票房榜':
+      return usBox(count, start)
+    case '豆瓣电影新片榜':
+      return newMovies(count, start)
+    case '豆瓣电影本周口碑榜':
+      return weekly(count, start)
+    default :
+      return new Promise((resolve, reject) => {
+        reject('错误: 404')
+      })
+  }
+}
 const getters = {}
 
-const actions = {}
+const actions = {
+  getMoreData ({commit}, {title, count, start}) {
+    console.log(title + count + start)
+    moreAjax(title, count, start).then((moreCurrentMovie) => {
+      // 成功则commit后台接口的数据，并把NET_ERROR的数据置空，并把加载中的状态置为false。
+      console.log(moreCurrentMovie)
+      // 深拷贝函数
+      function deepCopy (arr) {
+        if (arr instanceof Array) {
+          var arr1 = []
+          for (var i = 0; i < arr.length; i++) {
+            arr1[i] = deepCopy(arr[i])
+          }
+          return arr1
+        } else if (arr instanceof Object) {
+          var obj = {}
+          for (var j in arr) {
+            obj[j] = deepCopy(arr[j])
+          }
+          return obj
+        } else {
+          return arr
+        }
+      }
+
+      var obj = deepCopy(this.state.moreMovie.moreCurrentMovie)
+      delete obj[title]
+      commit(types.MORE_CURRENT_MOVIE, Object.assign({[title]: moreCurrentMovie}, obj))
+      commit(types.NET_STATUS, '')
+    }).catch((error) => {
+      commit(types.NET_STATUS, error)
+    })
+  }
+}
 
 const mutations = {
   [types.MORE_CURRENT_MOVIE] (state, moreCurrentMovie) {
